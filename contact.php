cb838c87d10e23a64a712dad16d6dd0ecd1a0b5b
praveengsits17 <50230955+praveengsits17@users.noreply.github.com>
2020-07-06 13:37:47 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,22 +1,31 @@
 <?php
-
-$name= $_REQUEST['Name'];
-$email = $_REQUEST['Email'];
-$address = $_REQUEST['Address'];
-$phone = $_REQUEST['Phone'];
-$subject = $_REQUEST['Subject'];
-$description = $_REQUEST['Description'];
-
-if(empty($name)||empty($email)||empty($address)||empty($phone)||empty($subject)||empty($description))
-{
-    echo "Please fill all the fields";
+if (isset(($_POST['Name']) && ($_POST['Email']) && ($_POST['Phone']) && ($_POST['Subject']) && ($_POST['Address']) && ($_POST['Description'])){
+$name =$_POST['Name'];
+$email=$_POST['Email'];
+$to= 'infoaceusmle@gmail.com';
+$subject= "New Enquiry";
+$body = <html>
+            <body>
+                <hr>
+                <p> Name:<br>'.$name.'</p>
+                <p> Email:<br>'.$email.'</p>
+            </body>
+          </html>';
+ 
+ $headers = "From: ".$name." <".$email.">\r\n";
+ $headers = "Reply-To: ".$email."\r\n";
+ $headers = "NINE-Version: 1.0\r\n";
+ $headers = "Content-type: text/html; charset=utf-8";
+ 
+ $send = mail($to, $subject, $body, $headers);
+ 
+ if($send){
+    echo '<br>';
+    echo 'Thanks for Contacting';
 }
-else
-{
-    mail("infoaceusmle@gmail.com", "New Enquiry", $address, $phone, $subject, "From: $name <$email>");
-    echo "<script type='text/javascript'>alert('Your Message Sent Successfully');
-    window.his.log(-1);
-    </script>";
-}
+else{
+echo 'error';
 
+}
+}
 ?>
